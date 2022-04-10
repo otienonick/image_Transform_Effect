50329d259a1be510583afdd6f49b4830dde439aa
otienonick <nick.odongi@student.moringaschool.com>
2022-04-10 16:04:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    // typing animation
+    var typed = new Typed(".typing",{
+        strings:["Nick","Otieno"],
+        typeSpeed:180,
+        backSpeed:30,
+        loop:true
+    })
+
+});
