8c438257287caae449cb2c58bb4ae0e4f240e929
otienonick <nick.odongi@student.moringaschool.com>
2022-04-10 16:24:41 +0300

    add typing delay speed

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $( document ).ready(function() {
     // typing animation
     var typed = new Typed(".typing",{
         strings:["Nick","Otieno"],
-        typeSpeed:180,
+        typeSpeed:60,
+        backDelay: 3000,
         backSpeed:30,
         loop:true
     })
